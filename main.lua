21516cdd69c940c8ba28fa09aaccea3d404532b2
Jay Patel <jaypatel@Jays-MBP.fios-router.home>
2020-05-26 20:05:37 -0400

    Pong-0-Complete

--- main.lua
+++ main.lua
@@ -1,3 +1,30 @@
-function love.draw()
-    love.graphics.print("Hello World", 400, 300)
+
+--[[
+    Day 0
+]]
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+function love.load()
+    love.window.setMode(WINDOW_WIDTH,WINDOW_HEIGHT,{
+        fullscreen = false,
+        resizable = false,
+        vsync = true --[[ Synced to User screen]]
+    })
 end
+
+
+
+function love.draw()
+    love.graphics.printf(
+        "Hello World",
+        0,
+        WINDOW_HEIGHT/2 - 6, --[[ Default font is size 6, so adujust by 6 for perfect centering]]
+        WINDOW_WIDTH,
+        'center')
+end
+
+
+
+
+
+
